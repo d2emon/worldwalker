3a9d5659c7706cf225b11af514190b84b50e80a0
Dmitry Kutsenko <d2emonium@gmail.com>
2017-04-28 15:36:21 +0300

    Images

---
+++ src/player.py
@@ -0,0 +1,40 @@
+from pygame import sprite, Surface, Color, Rect
+
+
+MOVE_SPEED = 7
+WIDTH = 32
+HEIGHT = 32
+COLOR = "#888888"
+CONSTS = ((10, 758), (10, 588))
+
+
+class Player(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.xvel = 0
+        self.yvel = 0
+        self.startX = x
+        self.startY = y
+        self.image = Surface((WIDTH, HEIGHT))
+        self.image.fill(Color(COLOR))
+        self.rect = Rect(x, y, WIDTH, HEIGHT)
+
+    def update(self, xvel, yvel):
+        self.xvel = xvel * MOVE_SPEED
+        self.yvel = yvel * MOVE_SPEED
+        if (xvel == 0) and (yvel == 0):
+            self.xvel = 0
+            self.yvel = 0
+        self.rect.x += self.xvel
+        self.rect.y += self.yvel
+        if self.rect.x <= CONSTS[0][0]:
+            self.rect.x = CONSTS[0][0] + 1
+        if self.rect.x >= CONSTS[0][1]:
+            self.rect.x = CONSTS[0][1] - 1
+        if self.rect.y <= CONSTS[1][0]:
+            self.rect.y = CONSTS[1][0] + 1
+        if self.rect.y >= CONSTS[1][1]:
+            self.rect.y = CONSTS[1][1] - 1
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
--- src/run.py
+++ src/run.py
@@ -1,17 +1,71 @@
 #! /usr/bin/python
-import pygame, sys
+import config
+import pygame
 from pygame.locals import *
+from player import Player
+from pygame.time import Clock
+
+
+CONTROLS = {
+    K_LEFT: (-1, None),
+    K_RIGHT: (1, None),
+    K_UP: (None, -1),
+    K_DOWN: (None, 1),
+}
 
 
 def main():
     pygame.init()
-    DISPLAYSURF = pygame.display.set_mode((400, 300))
+    screen = pygame.display.set_mode(config.DISPLAY)
     pygame.display.set_caption('Hello World Play!')
+
+    bg_img = pygame.image.load('../res/global/map.jpg')
+    bg = pygame.Surface((config.WIN_WIDTH, config.WIN_HEIGHT))
+    # bg.fill(pygame.Color(config.BACKGROUND_COLOR))
+    bg.blit(bg_img, (0, 0))
+
+    timer = Clock()
+
+    hero = Player(10, 10)
+    xvel = yvel = 0
+
     while True:
+        timer.tick(60)
         for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+            event_exit(event)
+            xvel, yvel = event_move(event, xvel, yvel)
+
+        screen.blit(bg, (0, 0))
+
+        hero.update(xvel, yvel)
+        hero.draw(screen)
+
+        pygame.display.update()
+
+
+def event_exit(event):
+    if event.type == QUIT:
+        pygame.quit()
+        raise SystemExit("QUIT")
+    if event.type == KEYDOWN and event.key == K_ESCAPE:
+        raise SystemExit("QUIT")
+
+
+def event_move(event, xvel, yvel):
+    if event.type == KEYDOWN and event.key in CONTROLS.keys():
+        c = CONTROLS[event.key]
+        if c[0] is not None:
+            xvel = c[0]
+        if c[1] is not None:
+            yvel = c[1]
+
+    if event.type == KEYUP and event.key in CONTROLS.keys():
+        c = CONTROLS[event.key]
+        if c[0] is not None:
+            xvel = 0
+        if c[1] is not None:
+            yvel = 0
+    return xvel, yvel
 
 
 if __name__ == "__main__":
