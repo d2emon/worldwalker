fea5ad3c7d27b37bd2bfdbcfac09d46e457522af
Dmitry Kutsenko <d2emonium@gmail.com>
2019-06-21 12:55:35 +0300

    NDebug

---
+++ src/gamelib/temp_mud/ndebug/__init__.py
@@ -0,0 +1,13 @@
+MAX_USERS = 16
+
+
+def delu2():
+    yield "Selection from main menu only\n"
+
+
+def chpwd():
+    yield "To change your password select option 2 from the main menu\n"
+
+
+def debug2():
+    yield "No debugger available\n"
